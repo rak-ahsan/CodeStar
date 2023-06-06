6435d02dee72804a617a42cb5f6bfe122d912021
Rakib Ahsan <rakibzubayar@gmail.com>
2023-06-07 01:08:40 +0600

    defign

---
+++ config/sidebar.php
@@ -0,0 +1,9 @@
+<div class="container-fluid d-flex justify-content-start gx-0">
+    <div class="col-2  vh-100 text-white" style="background-color: #123456;">
+      <a class = "nav-link p-3 border border-bottom-1 border-white" href="#">Dashboard</a>
+      <a class = "nav-link p-3 " href="#">Add New Agent</a>
+      <a class = "nav-link p-3 " href="#">Total Profit</a>
+      <a class = "nav-link p-3 " href="#">Avaiable Property</a>
+      <a class = "nav-link p-3 " href="#">Running Project</a>
+      <a class = "nav-link p-3 " href="#">Total agent</a>
+    </div>
