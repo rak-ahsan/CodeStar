f880237afed13e9578be4294d31bf132b64e5509
Rakib Ahsan <rakibzubayar@gmail.com>
2023-06-12 12:52:09 -0700

    new add

---
+++ pages/suplier/viewformsuplier.php
@@ -0,0 +1,62 @@
+<?php include('../../includes/conf.php');
+  get_header();
+  get_side();
+?>
+<div class="col-md-10 table-responsive p-3">
+<?php 
+    $sql = "SELECT * FROM suplier"; 
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    ?>
+        <table class='table table-light align-middle text-center table-bordered'>
+            <thead>
+                <tr> 
+                    <th>suplier Name</th> 
+                    <th>suplier Contatc</th>
+                    <th>suplier Email</th>
+                    <th>Total Amount </th>
+                    <th>Total Paid</th>
+                    <th>Total Due</th>
+                    <th>Date</th>
+                    <!-- <th>Photo</th> -->
+                    <th colspan='2'>Action</th>
+                </tr>
+            </thead>
+       <?php while ($row = $result->fetch_assoc()) {?>
+            <tbody>
+            <tr>
+                <td><?=$row['sup_name']?></td>
+                <td><?=$row['sup_contact_no']?></td>
+                <td><?=$row['sup_email']?></td>
+                <td><?=$row['tamount']?></td>
+                <td><?=$row['tpaid']?></td>
+                <td><?=$row['tdue']?></td>
+                <td><?=$row['submission_date']?></td>
+                <!-- <td>
+                     <?php if($row['land_img']!=''){ 
+                    // echo "<img height='50' src='../../dist/images/land/$row[land_img]'>";
+                   }else{ 
+                    //  echo '<img height="50" src="../../dist/images/pic/avatar.png" alt="Image">';
+                   }
+                   ?>
+                </td> -->
+                <td>
+
+                  <a class='btn nav-link' href='supupdatedata.php?id=<?=$row['sup_id']?>'>
+                  <i class='fa-regular fa-pen-to-square fa-xl'></i></a>
+                   </td>
+                
+                <td>
+                  <a class='btn nav-link' href='Delete.php?id=<?=$row['sup_id']?>'>
+                  <i class='fa-solid fa-trash fa-xl' style='color: #ff0000;'></i></a>
+                </td>
+            <tr>
+            
+            
+            </tbody>
+            <?php }?>
+        </table>
+        <?php }
+                
+        ?>
