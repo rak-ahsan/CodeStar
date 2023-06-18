77d7f9ca5c895b1018de70c656eafe4a7a940c98
rak-ahsan <rakibzubayar@gmail.com>
2023-06-18 13:24:14 +0600

    sql

--- pages/index/index.php
+++ pages/index/index.php
@@ -3,53 +3,31 @@
   get_side();
 ?>
 <link rel="stylesheet" href="../../dist/css/cus.css">
-<div class="container col-md-10 h-50 justify-content-center">
-  <span>Dashboard</span>
-<div class="d-flex">
-  <div class="col-sm-4 h-25">
-    <div class="box1  ">
-    <div class="card mt-3 text-center">
-        <div class="card-header bg-success text-white">
-          <h6>Total Projects</h6>
+<div class="col-md-10">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="p-3 d-flex flex-column">
+                    <span>
+
+                    <i class="fa-solid fa-chart-column fa-beat" style="color: #ff8000;"></i>
+
+</span>
+                    <span>Total Calculation</span>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex">
+            <div class="p-3 d-flex flex-column">
+                    <span>Total sales</span>
+                    <span>Total sales</span>
+                </div>
+                <div class="p-3 d-flex flex-column">
+                    <span>Dashboard</span>
+                    <span>Total Profit</span>
+                </div>
+                <div class="p-3 d-flex flex-column">
+                    <span>Dashboard</span>
+                    <span>Total loss</span>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-          <h5>500</h5>
-        </div>
-      </div>
     </div>
-  </div>
-  <div class="col-sm-4 h-25">
-    <div class="box1  text-center">
-    <div class="card mt-3 ">
-        <div class="card-header bg-success text-white">
-          <h6>
-          Total Agents
-          </h6>
-        </div>
-        <div class="card-body">
-          <h5>25</h5>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4 h-25">
-    <div class="box1  text-center">
-    <div class="card mt-3 ">
-        <div class="card-header bg-success text-white">
-          <h6>Total Profit</h6>
-        </div>
-        <div class="card-body">
-          <h5>5000000$</h5>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="container vh-50 mt-5">
-  <div class="col-md-12">
-    <div class="chart">
-    <div id="piechart" class="w-50 col-6">
-    </div>
-  </div>
-</div>
-</div>
